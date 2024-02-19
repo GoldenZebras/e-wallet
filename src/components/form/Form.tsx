86b85d178fd56378f9fef5c00386a625c652c1cb
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2024-02-19 14:40:06 +0100

    deleted variables

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,28 +2,22 @@ import Button from '../button/Button';
 import './form.scss';
 
 const Form = () => {
-
-    let cardNumber;
-    let name;
-    let vallid;
-    let ccv;
-
     return(
         <section className='form'>
             <label className='form-subtitles'>Card number</label>
-            <input type='text' className='input' value={cardNumber}/>
+            <input type='text' className='input'/>
 
             <label className='form-subtitles'>Cardholder name</label>
-            <input type='text' placeholder='FIRSTNAME LASTNAME' className='input' value={name}/>
+            <input type='text' placeholder='FIRSTNAME LASTNAME' className='input'/>
 
             <article className='valid-ccv'>
                 <div className='form-subtitles'>
                     <label>Valid thru</label><br></br>
-                    <input type='text' className='input' value={vallid} />
+                    <input type='text' className='input'/>
                 </div>
                 <div className='form-subtitles'>
                     <label>CCV</label><br></br>
-                    <input type='text' className='input' value={ccv} />
+                    <input type='text' className='input' />
                 </div>
             </article>
 
