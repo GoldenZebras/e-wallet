bfa5996ff030898dde6f3de97b7f68ccce7fe15c
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2024-02-19 10:57:41 +0100

    styled /addcard layout

--- src/pages/AddCard.tsx
+++ src/pages/AddCard.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import './addCard.scss';
 
 const AddCard = () => {
-  return <div>AddCard</div>;
+  return (
+    <main className="main-add-cards">
+      <div className="title">Add a new bank card</div>
+
+      <p className="new-card-title">New card</p>
+      
+      {/* <Card /> */}
+
+    </main>
+  );
 };
 
 export default AddCard;
