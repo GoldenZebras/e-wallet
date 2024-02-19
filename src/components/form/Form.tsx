4983b559db98c805fc50edbefdf9b9cebf4f7f94
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2024-02-19 14:29:33 +0100

    added value tags

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,22 +2,28 @@ import Button from '../button/Button';
 import './form.scss';
 
 const Form = () => {
+
+    let cardNumber;
+    let name;
+    let vallid;
+    let ccv;
+
     return(
         <section className='form'>
             <label className='form-subtitles'>Card number</label>
-            <input type='text' className='input'/>
+            <input type='text' className='input' value={cardNumber}/>
 
             <label className='form-subtitles'>Cardholder name</label>
-            <input type='text' placeholder='FIRSTNAME LASTNAME' className='input'/>
+            <input type='text' placeholder='FIRSTNAME LASTNAME' className='input' value={name}/>
 
             <article className='valid-ccv'>
                 <div className='form-subtitles'>
                     <label>Valid thru</label><br></br>
-                    <input type='text' className='input'/>
+                    <input type='text' className='input' value={vallid} />
                 </div>
                 <div className='form-subtitles'>
                     <label>CCV</label><br></br>
-                    <input type='text' className='input'/>
+                    <input type='text' className='input' value={ccv} />
                 </div>
             </article>
 
