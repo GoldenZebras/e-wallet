7ba97fdf8c7844b2afccb53fb9eedbfe5352b9de
daniel-budai <danielbudai@tutanota.com>
2024-02-20 15:01:45 +0100

    emv chip added to card, chaged font

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { CardProps } from '../../models/CardProps';
-import './card.scss';
+import React from "react";
+import { CardProps } from "../../models/CardProps";
+import "./card.scss";
 
-const Card: React.FC<CardProps> = ({ cardHolder, cardNumber, expirationDate, vendor }) => {
+const Card: React.FC<CardProps> = ({
+  cardHolder,
+  cardNumber,
+  expirationDate,
+  vendor,
+}) => {
   function formatCardNumber(cardNumber: string) {
     while (cardNumber.length < 16) {
-      cardNumber += 'X';
+      cardNumber += "X";
     }
-    return cardNumber.replace(/(.{4})/g, '$1 ').trim();
+    return cardNumber.replace(/(.{4})/g, "$1 ").trim();
   }
 
   return (
@@ -16,6 +21,7 @@ const Card: React.FC<CardProps> = ({ cardHolder, cardNumber, expirationDate, ven
       <div className="card__holder">{cardHolder}</div>
       <div className="card__expiry">{expirationDate}</div>
       <figure className={`card__vendor ${vendor}`}></figure>
+      <figure className={`card__emv ${vendor}`}></figure>
     </div>
   );
 };
